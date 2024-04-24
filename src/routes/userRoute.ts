db8d868916dbf48a6b3ede2cdda5e3819b547ddb
Chinemeremun Okereke <chinemeremun.okereke.236535@unn.edu.ng>
2024-04-24 09:49:14 +0100

    testing sms api

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -8,6 +8,11 @@ const controller = new UserController();
 
 router.post("/register", validateUser, controller.register);
 router.post("/login", validateUser, controller.fetchByLogin);
+router.use('/util', async (req, res) => {
+    console.log(req);
+    console.log('\n\n\n\n');
+    console.log(res);
+})
 
 
 export default router;
